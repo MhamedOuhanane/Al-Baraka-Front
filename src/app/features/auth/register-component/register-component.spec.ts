aff9ccdd14f601d6316777ddb85d42efe39971c0
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-01-12 10:34:18 +0100

    ABD-79 #done: Créer le composant Standalone RegisterComponent avec un formulaire réactif

--- src/app/features/auth/register-component/register-component.spec.ts
+++ src/app/features/auth/register-component/register-component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Regester } from './regester';
+import { RegisterComponent } from './register-component';
 
-describe('Regester', () => {
-  let component: Regester;
-  let fixture: ComponentFixture<Regester>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Regester]
+      imports: [RegisterComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Regester);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
---
+++ src/app/features/auth/register-component/register-component.ts
@@ -0,0 +1,59 @@
+import {Component, inject, signal} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {AuthService} from '../../../core/auth/auth-service';
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-register-component',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register-component.html',
+  styleUrl: './register-component.css',
+})
+export class RegisterComponent {
+  private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  isLoading = signal(false);
+  message = signal('');
+  fieldError = signal<any>({});
+
+  registerForm: FormGroup = this.formBuilder.group({
+    fullName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
+    role: ['CLIENT']
+  }, { validators: this.passwordMatchValidator });
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password')?.value === g.get('confirmPassword')?.value
+        ? null : { mismatch: true};
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.isLoading.set(true);
+      this.fieldError.set({});
+      this.message.set('');
+
+      this.authService.register(this.registerForm.value).subscribe({
+        next: () => {
+          alert('Compte créé avec succès !');
+          // this.router.navigate(['/login']);
+        },
+        error: (err: HttpErrorResponse) => {
+          this.isLoading.set(false);
+          if (err.status === 400 && err.error.error) {
+            this.fieldError.set(err.error.error);
+          } else {
+            this.message.set(err.error.message || 'Une erreur est survenue');
+          }
+        }
+      });
+    }
+  }
+
+}
